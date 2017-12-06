bbbd17612736060d7a9aeec9a90ddf8d26ea4188
Christian Cuadros Betancur <christian@credesa.cl>
2017-12-06 11:01:16 -0300

    Commit final

---
+++ SquareGame/Controllers/TableroController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SquareGame.Controllers
+{
+    public class TableroController : Controller
+    {
+        // GET: Tablero
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Mejorado()
+        {
+            return View();
+        }
+        // Método de nombre Bienvenida, que no recibe parámetros y retorna una variable de tipo string, en este caso devolverá un string con etiquetas HTML en su contenido
+        public string Bienvenida(string id)
+        {
+            return "<h1> Hola Mundo" + id + "</h1>";
+        }
+    }
+}
